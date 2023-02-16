0a8eb31d1b565992ca36d5bb3c074466bd91dea3
Leonard Papenmeier <leonard.papenmeier@posteo.de>
2023-02-16 17:57:44 +0100

    add lunarlander and robotpushing

--- main.py
+++ main.py
@@ -4,33 +4,42 @@ import torch
 
 from benchsuite import settings, SVM
 from benchsuite import LassoDNA, LassoSimple, LassoMedium, LassoHard, LassoHigh
+from benchsuite.lunarlander import LunarLanderBenchmark
 from benchsuite.mopta08 import Mopta08
+from benchsuite.robotpushing import RobotPushingBenchmark
 
 if __name__ == '__main__':
+
+    benchmark_options = dict(
+        lasso_dna=LassoDNA,
+        lasso_simple=LassoSimple,
+        lasso_medium=LassoMedium,
+        lasso_hard=LassoHard,
+        lasso_high=LassoHigh,
+        svm=SVM,
+        mopta08=Mopta08,
+        lunarlander=LunarLanderBenchmark,
+        robotpushing=RobotPushingBenchmark,
+    )
+
     parser = argparse.ArgumentParser(
         prog='BenchSuite',
         description='Provides some benchmarks',
-        epilog='Enjoy the program!')
-    parser.add_argument("--name", help="Name of the benchmark", type=str, required=True)
+        epilog='Enjoy the program!'
+    )
+    parser.add_argument(
+        "--name",
+        help=f"Name of the benchmark. Options: {list(benchmark_options.keys())}",
+        type=str,
+        required=True
+        )
     parser.add_argument("--x", "-x", help="The point", type=float, nargs="+", required=True)
     args = parser.parse_args()
 
-    if args.name == "lasso_dna":
-        bench = LassoDNA()
-    elif args.name == "lasso_simple":
-        bench = LassoSimple()
-    elif args.name == "lasso_medium":
-        bench = LassoMedium()
-    elif args.name == "lasso_hard":
-        bench = LassoHard()
-    elif args.name == "lasso_high":
-        bench = LassoHigh()
-    elif args.name == "svm":
-        bench = SVM()
-    elif args.name == "mopta08":
-        bench = Mopta08()
-    else:
+    if args.name not in benchmark_options:
         raise RuntimeError(f"Unknown benchmark {args.name}")
+    else:
+        bench = benchmark_options[args.name]()
 
     x = torch.tensor(args.x, dtype=settings.DTYPE, device=settings.DEVICE)
 
