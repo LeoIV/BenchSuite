add mujoco benchmarks and correct scaling

main.py
@@ -2,10 +2,10 @@ import argparse
 
 import torch
 
-from benchsuite import settings, SVM
-from benchsuite import LassoDNA, LassoSimple, LassoMedium, LassoHard, LassoHigh
+from benchsuite import LassoDNA, LassoHard, LassoHigh, LassoMedium, LassoSimple, SVM, settings
 from benchsuite.lunarlander import LunarLanderBenchmark
 from benchsuite.mopta08 import Mopta08
+from benchsuite.mujoco import MujocoAnt, MujocoHalfCheetah, MujocoHopper, MujocoHumanoid, MujocoSwimmer, MujocoWalker
 from benchsuite.robotpushing import RobotPushingBenchmark
 
 if __name__ == '__main__':
@@ -20,6 +20,12 @@ if __name__ == '__main__':
         mopta08=Mopta08,
         lunarlander=LunarLanderBenchmark,
         robotpushing=RobotPushingBenchmark,
+        swimmer=MujocoSwimmer,
+        humanoid=MujocoHumanoid,
+        ant=MujocoAnt,
+        hopper=MujocoHopper,
+        walker=MujocoWalker,
+        halfcheetah=MujocoHalfCheetah,
     )
 
     parser = argparse.ArgumentParser(
@@ -32,7 +38,7 @@ if __name__ == '__main__':
         help=f"Name of the benchmark. Options: {list(benchmark_options.keys())}",
         type=str,
         required=True
-        )
+    )
     parser.add_argument("--x", "-x", help="The point", type=float, nargs="+", required=True)
     args = parser.parse_args()
 
@@ -42,6 +48,7 @@ if __name__ == '__main__':
         bench = benchmark_options[args.name]()
 
     x = torch.tensor(args.x, dtype=settings.DTYPE, device=settings.DEVICE)
-
+    # scale x to the correct range
+    x = bench.lb + (bench.ub - bench.lb) * x
     y = bench(x)
     print(y.detach().cpu().numpy().tolist()[0])
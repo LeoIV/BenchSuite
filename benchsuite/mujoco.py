4511e883f4a1ef0136e66f1539ebf30143220e44
Leonard Papenmeier <leonard.papenmeier@posteo.de>
2023-02-17 14:16:18 +0100

    add mujoco benchmarks and correct scaling

---
+++ benchsuite/mujoco.py
@@ -0,0 +1,103 @@
+import os
+from pathlib import Path
+from typing import Any
+
+import torch
+
+from benchsuite import settings
+from benchsuite.benchmark import Benchmark
+from benchsuite.utils.mujoco import func_factories
+
+
+class MujocoBenchmark(Benchmark):
+
+    def __init__(
+        self,
+        dim: int,
+        ub: torch.Tensor,
+        lb: torch.Tensor,
+        benchmark: Any
+    ):
+        super().__init__(dim=dim, lb=lb, ub=ub)
+        self.benchmark = benchmark
+
+    def __call__(
+        self,
+        x: torch.Tensor
+    ) -> torch.Tensor:
+        if x.ndim == 1:
+            x = x.unsqueeze(0)
+        y = self.benchmark(x)[0]
+        return -torch.tensor(y)
+
+
+class MujocoSwimmer(MujocoBenchmark):
+    def __init__(
+        self
+    ):
+        super().__init__(
+            dim=16,
+            ub=torch.ones(16, dtype=settings.DTYPE, device=settings.DEVICE),
+            lb=-1 * torch.ones(16, dtype=settings.DTYPE, device=settings.DEVICE),
+            benchmark=func_factories["swimmer"].make_object()
+        )
+
+
+class MujocoHumanoid(MujocoBenchmark):
+    def __init__(
+        self
+    ):
+        super().__init__(
+            dim=6392,
+            ub=torch.ones(6392, dtype=settings.DTYPE, device=settings.DEVICE),
+            lb=-1 * torch.ones(6392, dtype=settings.DTYPE, device=settings.DEVICE),
+            benchmark=func_factories["humanoid"].make_object()
+        )
+
+
+class MujocoAnt(MujocoBenchmark):
+    def __init__(
+        self
+    ):
+        super().__init__(
+            dim=888,
+            ub=torch.ones(888, dtype=settings.DTYPE, device=settings.DEVICE),
+            lb=-1 * torch.ones(888, dtype=settings.DTYPE, device=settings.DEVICE),
+            benchmark=func_factories["ant"].make_object()
+        )
+
+
+class MujocoHopper(MujocoBenchmark):
+    def __init__(
+        self
+    ):
+        super().__init__(
+            dim=33,
+            ub=1.4 * torch.ones(33, dtype=settings.DTYPE, device=settings.DEVICE),
+            lb=-1.4 * torch.ones(33, dtype=settings.DTYPE, device=settings.DEVICE),
+            benchmark=func_factories["hopper"].make_object()
+        )
+
+
+class MujocoWalker(MujocoBenchmark):
+    def __init__(
+        self
+    ):
+        super().__init__(
+            dim=102,
+            ub=0.9 * torch.ones(102, dtype=settings.DTYPE, device=settings.DEVICE),
+            lb=-1.8 * torch.ones(102, dtype=settings.DTYPE, device=settings.DEVICE),
+            benchmark=func_factories["walker_2d"].make_object()
+        )
+
+
+class MujocoHalfCheetah(MujocoBenchmark):
+    def __init__(
+        self
+    ):
+        super().__init__(
+            dim=102,
+            ub=torch.ones(102, dtype=settings.DTYPE, device=settings.DEVICE),
+            lb=-1.0 * torch.ones(102, dtype=settings.DTYPE, device=settings.DEVICE),
+            benchmark=func_factories["half_cheetah"].make_object()
+        )
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import argparse
 
 import torch
 
-from benchsuite import settings, SVM
-from benchsuite import LassoDNA, LassoSimple, LassoMedium, LassoHard, LassoHigh
+from benchsuite import LassoDNA, LassoHard, LassoHigh, LassoMedium, LassoSimple, SVM, settings
 from benchsuite.lunarlander import LunarLanderBenchmark
 from benchsuite.mopta08 import Mopta08
+from benchsuite.mujoco import MujocoAnt, MujocoHalfCheetah, MujocoHopper, MujocoHumanoid, MujocoSwimmer, MujocoWalker
 from benchsuite.robotpushing import RobotPushingBenchmark
 
 if __name__ == '__main__':
@@ -20,6 +20,12 @@ if __name__ == '__main__':
         mopta08=Mopta08,
         lunarlander=LunarLanderBenchmark,
         robotpushing=RobotPushingBenchmark,
+        swimmer=MujocoSwimmer,
+        humanoid=MujocoHumanoid,
+        ant=MujocoAnt,
+        hopper=MujocoHopper,
+        walker=MujocoWalker,
+        halfcheetah=MujocoHalfCheetah,
     )
 
     parser = argparse.ArgumentParser(
@@ -32,7 +38,7 @@ if __name__ == '__main__':
         help=f"Name of the benchmark. Options: {list(benchmark_options.keys())}",
         type=str,
         required=True
-        )
+    )
     parser.add_argument("--x", "-x", help="The point", type=float, nargs="+", required=True)
     args = parser.parse_args()
 
@@ -42,6 +48,7 @@ if __name__ == '__main__':
         bench = benchmark_options[args.name]()
 
     x = torch.tensor(args.x, dtype=settings.DTYPE, device=settings.DEVICE)
-
+    # scale x to the correct range
+    x = bench.lb + (bench.ub - bench.lb) * x
     y = bench(x)
     print(y.detach().cpu().numpy().tolist()[0])
